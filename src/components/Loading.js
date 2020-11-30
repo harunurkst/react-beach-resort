42dba3d3a863e86b5b09e445d570079184864e6f
harunurkst <harun1393@gmail.com>
2020-11-30 15:33:36 +0600

    build fix

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -5,7 +5,7 @@ export default function Loading() {
     return (
         <div className="loading">
             <h4>Room data loading...</h4>
-            <img src={loadingGif} />
+            <img src={loadingGif} alt=""/>
         </div>
     )
 }
